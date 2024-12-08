e44776099f57ed1a09d2fe3eccc2fee1e7488557
lukamskh <mskhaladze.luka7@students.gov.ge>
2024-12-08 18:00:42 +0400

    commit

---
+++ PORTFOLIO/why.js
@@ -0,0 +1,5 @@
+const hideButton = document.getElementById('show_it1');
+const myImage = document.getElementById('photo');
+show_it().addEventListener('click', function() {
+    myImage.style.display = 'none';
+});
