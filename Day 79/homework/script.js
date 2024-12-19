e4762a98848f15ac9ada086e78d18ac0cecba035
lukamskh <mskhaladze.luka7@students.gov.ge>
2024-12-19 14:38:26 +0400

    commit

---
+++ Day 79/homework/script.js
@@ -0,0 +1,27 @@
+alert(1+1);
+console.log(1+1);
+alert(2-2);
+console.log(2-2);
+alert(3/3);
+console.log(3/3)
+alert(4*4);
+console.log(4*4);
+alert(5**5);
+console.log(5**5);
+alert(2+1);
+console.log(2+1);
+alert(3-2);
+console.log(3-2);
+alert(4/3);
+console.log(4/3)
+alert(5*4);
+console.log(5*4);
+alert(6**5);
+console.log(6**5);
+let num = 11
+
+for (let i = 0; i < 11; i++) {
+    num = num - 1
+    alert(`The world will be destroyed in ${num} second`)
+    console.log(`The world will be destroyed in ${num} second`)
+}
