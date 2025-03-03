b4b945215a42efb0918c8ee80521e5ec96cf4c11
lukamskh <mskhaladze.luka7@students.gov.ge>
2025-03-03 21:09:09 +0400

    commit

--- Day 109/homework/script.js
+++ Day 109/homework/script.js
@@ -1,1 +1,10 @@
 document.body.innerHTML = `<button>SUP???</button>`;
+
+function priceUp(prices, increase) {
+  for (let i = 0; i < prices.length; i++) {
+    prices[i] = prices[i] + increase;
+  }
+  console.log(prices);
+}
+
+priceUp([10, 11, 12], 10);
