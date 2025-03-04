fea26c62f43e942db455bea2fdc3de0219253b82
lukamskh <mskhaladze.luka7@students.gov.ge>
2025-03-04 22:57:30 +0400

    commit

---
+++ Day 111/classwork/script.js
@@ -0,0 +1,13 @@
+let img1 = document.getElementById("fruitImage");
+
+function banana1() {
+  img1.src = "banana.png";
+}
+
+function pear1() {
+  img1.src = "pear.png";
+}
+
+function strawberry1() {
+  img1.src = "strawberry.png";
+}
